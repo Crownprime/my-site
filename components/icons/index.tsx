2d3c09d1d3f9b112dbc6c4ff32f5e8679aa27c72
jiangxujin <jiangxujin@bytedance.com>
2021-10-19 19:10:35 +0800

    feat: home animate

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -1,5 +1,7 @@
 import Home from './home'
 import Dashboard from './dashboard'
+import ExpandMore from './expand-more'
 
 export const HomeIcon = Home
 export const DashboardIcon = Dashboard
+export const ExpandMoreIcon = ExpandMore
--- features/home/cover.tsx
+++ features/home/cover.tsx
@@ -1,16 +1,41 @@
+import { animateScroll as scroll } from 'react-scroll'
+import { ExpandMoreIcon } from 'components/icons'
 import WriteText from 'components/write-text'
+import { useScrollY } from 'hooks'
+import styles from './styles.module.scss'
 
 const Cover = () => {
+  const y = useScrollY()
+  const handleClick = () => {
+    const windowH =
+      document.documentElement.clientHeight || document.body.clientHeight
+    scroll.scrollTo(windowH)
+  }
   return (
-    <div className="home-cover">
+    <div className={styles.homeCover}>
       <WriteText />
+      {Boolean(y < 100) && (
+        <div className={styles.homeCoverExpand}>
+          <div className="expand-animate" onClick={handleClick}>
+            <ExpandMoreIcon />
+          </div>
+        </div>
+      )}
       <style jsx>{`
-        .home-cover {
-          width: 100%;
-          height: 100vh;
-          display: flex;
-          align-items: center;
-          justify-content: center;
+        .expand-animate {
+          animation-name: shakeY;
+          animation-iteration-count: infinite;
+          animation-duration: 2s;
+          cursor: pointer;
+        }
+        @keyframes shakeY {
+          from,
+          to {
+            transform: translateY(0);
+          }
+          50% {
+            transform: translateY(-10px);
+          }
         }
       `}</style>
     </div>
