0eb73334da449ae5b53aa34c2b9f7a1f043960fa
jiangxujin <jiangxujin@bytedance.com>
2021-11-01 20:00:33 +0800

    style: 优化首页样式

--- src/components/posts-list/index.tsx
+++ src/components/posts-list/index.tsx
@@ -2,27 +2,19 @@ import React from 'react'
 import Link from 'next/link'
 import { echoTime } from '@/lib'
 import { PostsData } from 'apis/posts'
-import { PostSListItemWrap } from './styleComponents'
-import Container from '@/components/layout/container'
+import { PostListStyled, PostItemStyled } from './styled'
 
 const PostsList: React.FC<{
   data: PostsData
 }> = ({ data }) => {
   return (
-    <Container>
-      <div style={{ background: '#fff', borderRadius: '4px' }}>
-        {data.map(({ title, sub, date, tags, id }) => (
-          <PostSListItemWrap
-            key={id}
-            sub={sub}
-            date={echoTime(date)}
-            tags={tags}
-          >
-            <Link href={'/post/' + id}>{title}</Link>
-          </PostSListItemWrap>
-        ))}
-      </div>
-    </Container>
+    <PostListStyled>
+      {data.map(({ title, sub, date, tags, id }) => (
+        <PostItemStyled key={id} sub={sub} date={echoTime(date)} tags={tags}>
+          <Link href={'/post/' + id}>{title}</Link>
+        </PostItemStyled>
+      ))}
+    </PostListStyled>
   )
 }
 
