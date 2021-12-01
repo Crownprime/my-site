5fa76e3a212d580dac79c3b8ac54d7bd76d5d253
jiangxujin <jiangxujin@bytedance.com>
2021-12-01 11:29:37 +0800

    test: 修改lockfile

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,16 +1,7 @@
-import { FC } from 'react'
-import PostsList from '@/components/posts-list'
-import PostTimeLine from '@/components/post-time-line'
 import Cover from './cover'
 
 const Home = () => {
   return <Cover />
-  // return (
-  //   <main>
-  //     <Cover />
-  //     <PostTimeLine data={postsData} />
-  //   </main>
-  // )
 }
 
 export default Home
