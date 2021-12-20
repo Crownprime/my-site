74367494614ee654d1e1915e7c20d4ced725a0cd
jiangxujin <jiangxujin@bytedance.com>
2021-12-20 17:59:44 +0800

    fix: post text

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const withTM = require('next-transpile-modules')(['remark-html'])
  * @type {import('next').NextConfig}
  */
 const nextConfig = withTM({
+  swcMinify: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
   sassOptions: {
     prependData: `@import "@/styles/variables.scss";@import "@/styles/theme.scss";`,
   },
